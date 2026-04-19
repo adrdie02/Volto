b105e9987a6843017d6e6bd6513e9eb7a8d0c3cb
adrdie02 <adrdie02@gmail.com>
2026-04-19 21:38:39 +0200

    Set basename for BrowserRouter to '/Volto'

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename:"/Volto">
         <AuthProvider>
           <CartProvider>
             <CartDrawer />
