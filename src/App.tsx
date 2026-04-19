72b2cae9b9a965d5e7b543a43ab18208321c8b04
adrdie02 <adrdie02@gmail.com>
2026-04-19 21:39:32 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename:"/Volto">
+      <BrowserRouter basename="/Volto">
         <AuthProvider>
           <CartProvider>
             <CartDrawer />
